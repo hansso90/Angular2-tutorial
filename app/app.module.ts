feb2bff84c2f2f14b4bcb8f67c1752536e92c568
Hans Lemkes <hanslemkes@gmail.com>
2016-12-11 22:13:59 +0100

    Basis van Angular 2

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,40 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpModule } from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+import './rxjs-extensions';
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component'
+import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ 
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+    ],
+  declarations: [ 
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent
+     ],
+  providers: [
+    HeroService
+  ],
+  bootstrap:    [ 
+    AppComponent 
+    ]
 })
 export class AppModule { }
